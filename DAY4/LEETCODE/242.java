789e1292629e5f1f312338b74d3247950c42a259
Anushri Vadivelu <anushriv2007@gmail.com>
2026-03-24 22:05:22 +0530

    Create 242.java

---
+++ DAY4/LEETCODE/242.java
@@ -0,0 +1,16 @@
+//242. Valid Anagram
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+        String m =String.valueOf(ss);
+        String n = String.valueOf(tt);
+        if(m.equals(n)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
