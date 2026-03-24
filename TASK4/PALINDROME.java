1db8d40895cba7f052c4a22528244d2f0810aae1
Anushri Vadivelu <anushriv2007@gmail.com>
2026-03-24 23:30:19 +0530

    Add files via upload

---
+++ TASK4/PALINDROME.java
@@ -0,0 +1,20 @@
+package task4;
+import java.util.*;
+public class PALINDROME {
+  public static boolean isPalindrome(String s) {
+        s= s.toLowerCase();
+        s=s.replaceAll("[^A-Za-z0-9]","");
+        int start=0,end=s.length()-1;
+        while(start<end){
+            if(s.charAt(start)!=s.charAt(end)){
+                return false;
+            }start++;
+            end--;
+        }return true;
+    }
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String s = sc.nextLine();
+      System.out.println(isPalindrome(s));
+    }
+}
