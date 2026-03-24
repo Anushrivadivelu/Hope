bff2e8637203e2b323a73962814bd2cd5066310c
Anushri Vadivelu <anushriv2007@gmail.com>
2026-03-24 22:43:24 +0530

    Create 58.java

---
+++ DAY5/LEETCODE/58.java
@@ -0,0 +1,8 @@
+//58. Length of Last Word
+class Solution {
+    public int lengthOfLastWord(String s){
+        String[] res = s.split(" ");
+        int len = res.length;
+        return res[len-1].length();
+    }
+}
