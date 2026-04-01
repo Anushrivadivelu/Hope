79d6c384f133f358d6889639f37302efa5dfcb92
Anushri Vadivelu <anushriv2007@gmail.com>
2026-04-01 18:44:30 +0530

    Add files via upload

---
+++ DAY11/Autoboxing.java
@@ -0,0 +1,13 @@
+package day11;
+
+public class Autoboxing {
+  public static void main(String[] args) {
+    /*int a = 10;
+    Integer obj =a;
+    System.out.println(a==obj);*/
+    Integer b = 1000;
+    Integer ob = 1000;
+    System.out.println(ob==b);
+    System.out.println(b.equals(ob));
+  }
+}
---
+++ DAY11/Unboxing.java
@@ -0,0 +1,9 @@
+package day11;
+
+public class Unboxing {
+  public static void main(String[] args) {
+    Integer a = 126;
+    int b = a;
+    System.out.println(b);
+  }
+}
