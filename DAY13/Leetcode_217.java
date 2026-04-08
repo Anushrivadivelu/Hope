f20922a55d8579fba4bb1ee14c9e1694f3c18cb6
Anushri Vadivelu <anushriv2007@gmail.com>
2026-04-08 08:27:44 +0530

    Create Leetcode_217.java

---
+++ DAY13/Leetcode_217.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> d = new HashSet<>();
+        for(int num : nums){
+            if(!d.add(num)) return true;
+        }return false;
+    }
+}
