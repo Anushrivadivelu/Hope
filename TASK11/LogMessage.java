4d27faed9d9ed7dee0edb857ba31afd009a3e068
Anushri Vadivelu <anushriv2007@gmail.com>
2026-04-14 15:52:49 +0530

    Update LogMessage.java

--- TASK11/LogMessage.java
+++ TASK11/LogMessage.java
@@ -1,25 +1,15 @@
 package task11;
-
 public class LogMessage implements Comparable<LogMessage> {
-  private String message;
-  private long time;
-  private String level;
-  public LogMessage(String message, String level) {
+  String message;
+  long time;
+  public LogMessage(String message) {
     this.message = message;
-    this.level = level;
     this.time = System.currentTimeMillis();
   }
-  public String getLevel() {
-    return level;
+  public String toString() {
+    return time + " : " + message;
   }
-  public long getTime() {
-    return time;
-  }
-  public String format() {
-    return "[" + level + "] [" + time + "] " + message;
-  }
-  @Override
   public int compareTo(LogMessage other) {
-    return Long.compare(this.time, other.time);
+    return (int)(this.time - other.time);
   }
 }
